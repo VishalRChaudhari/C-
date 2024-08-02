60a7bc8d3c63bc23e1efcd3764813102c097aa2e
VishalRChaudhari <vishalchaudhari7800@gmail.com>
2024-08-02 11:21:45 +0530

    added pure virtal functions

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -22,6 +22,51 @@ public:
     }
 };
 
+/*
+pure virtual function / abstract function
+- it is a virtual function whose implementation is not done in the base class
+- it is initialized with 0 ie. assigned value 0
+- it is used to create interface
+*/
+
+class shape
+{
+public:
+    virtual float area() = 0;
+};
+class rectangle : public shape
+{
+private:
+    int length, breadth;
+
+public:
+    rectangle() {};
+    rectangle(int l, int b)
+    {
+        this->length = l;
+        this->breadth = b;
+    }
+    float area()
+    {
+        return length * breadth;
+    }
+};
+class circle : public shape
+{
+private:
+    int radius;
+
+public:
+    circle(int r)
+    {
+        this->radius = r;
+    }
+    float area()
+    {
+        return 3.1425 * radius * radius;
+    }
+};
+
 int main()
 {
     derived d;
@@ -29,5 +74,16 @@ int main()
     base *b;
     b = new derived;
     b->display();
+
+    cout << endl;
+    cout << " Pure Virtual Functions.\n";
+    circle c(4);
+    cout << "Area of Circle is : " << c.area();
+    cout << endl;
+
+    shape *s;
+    s = new rectangle(4, 9);
+    cout << "Area of Rectangle : " << s->area();
+
     return 0;
 }
